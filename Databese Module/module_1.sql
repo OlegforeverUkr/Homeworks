135305a9bd56dca49ed53cca222a4ba1854d3f56
Oleh Shliapin <shlyapin8848@gmail.com>
2023-05-30 13:38:22 +0300

    MODULE

---
+++ Databese Module/module_1.sql
@@ -0,0 +1,55 @@
+create table users (id serial, name varchar(255), 
+pwd varchar(255), 
+email varchar(255), 
+gender varchar(1));
+
+insert into users (id, name, pwd, email, gender)
+values
+  (1, 'Vasya', '21341234qwfsdf', 'mmm@mmail.com', 'm'),
+  (2, 'Alex', '21341234', 'mmm@gmail.com', 'm'),
+  (3, 'Alexey', 'qq21341234Q', 'alexey@gmail.com', 'm'),
+  (4, 'Helen', 'MarryMeeee', 'hell@gmail.com', 'f'),
+  (5, 'Jenny', 'SmakeMyb', 'eachup@gmail.com', 'f'),
+  (6, 'Lora', 'burn23', 'tpicks@gmail.com', 'f');
+
+                     info
+-----------------------------------------------
+ This is Vasya, he has email mmm@mmail.com
+ This is Alex, he has email mmm@gmail.com
+ This is Alexey, he has email alexey@gmail.com
+ This is Helen, she has email hell@gmail.com
+ This is Jenny, she has email eachup@gmail.com
+ This is Lora, she has email tpicks@gmail.com
+(6 rows)
+
+
+select 
+  concat('This is ', name, ', ', 
+    case when gender = 'm' then 'he' else 'she' end,
+    ' has email ', email) as info
+from users;
+
+                     info
+-----------------------------------------------
+ This is Vasya, he has email mmm@mmail.com
+ This is Alex, he has email mmm@gmail.com
+ This is Alexey, he has email alexey@gmail.com
+ This is Helen, she has email hell@gmail.com
+ This is Jenny, she has email eachup@gmail.com
+ This is Lora, she has email tpicks@gmail.com
+(6 rows)
+
+select 'Gender information:' as gen
+union all
+select concat('We have ', count(*) filter (where gender = 'm'), ' boys!') 
+as gen from users
+union all
+select concat('We have ', count(*) filter (where gender = 'f'), ' girls!') 
+as gen from users;
+
+         gen
+---------------------
+ Gender information:
+ We have 3 boys!
+ We have 3 girls!
+(3 rows)
---
+++ Databese Module/module_2.sql
@@ -0,0 +1,14 @@
+SELECT name, COUNT(word) AS words
+FROM vocabulary v
+JOIN word w ON v.id = w.vocabulary_id
+GROUP BY v.name;
+
+  name   | words
+---------+-------
+ animals |    10
+ school  |    10
+ SF      |    10
+ human   |    10
+ nature  |    10
+(5 rows)
+
